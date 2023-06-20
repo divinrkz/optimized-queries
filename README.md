174bdc5dcec45a9911c343b7d9f8977b65d57f01
divinirakiza <irakizadivin@gmail.com>
2023-06-20 19:57:48 +0200

    feat: add availability query

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 This is a repositories for rare queries that have been optimized at your disposal.
 
 ### Queries.
-- Geospatial Indexing 
-- Availability
+- [Geospatial Indexing](https://github.com/divinirakiza/optimized-queries/tree/main/geospatial-indexing)
+- [Availability](https://github.com/divinirakiza/optimized-queries/tree/main/availability)
---
+++ availability/query.sql
@@ -0,0 +1,104 @@
+-- SET @checkInDate = '';
+-- SET @checkOutDate = '';
+-- SET @rooomTypeId = '';
+
+
+--Find available room given a checkInDate, checkOutDate, and roomTypeId
+
+
+SELECT 
+        rooms."id", rooms."roomTypeId", rooms."roomNumber",
+        least(
+          coalesce(
+            (
+              select 
+                DATE_PART(
+                  'day', 
+                  AGE(
+                    @checkInDate, bookings."checkOutDate"
+                  )
+                ) 
+              FROM 
+                bookings 
+                join room_bookings on bookings."id" = room_bookings."bookingId" 
+              WHERE 
+                bookings."checkOutDate" < @checkInDate
+                and room_bookings."roomId" = rooms."id" 
+              ORDER BY 
+                bookings."checkOutDate" DESC 
+              LIMIT 
+                1
+            ), 365
+          ), 
+          365
+        ) 
+        + 
+        least(
+          coalesce(
+            (
+              select 
+                DATE_PART(
+                  'day', 
+                  AGE(
+                    bookings."checkInDate", @checkOutDate
+                  )
+                ) 
+              FROM 
+                bookings 
+                join room_bookings on bookings."id" = room_bookings."bookingId" 
+              WHERE 
+                bookings."checkInDate" > @checkOutDate
+                and room_bookings."roomId" = rooms."id" 
+              ORDER BY 
+                bookings."checkInDate" ASC
+            ), 
+            365
+          ), 
+          365
+        ) as "gap" 
+      FROM 
+        rooms 
+        /* join rooms table with full detail (check in check out etc) */
+        left join (
+          select 
+            "checkInDate", 
+            "checkOutDate", 
+            "roomId" 
+          from 
+            room_bookings 
+            join bookings on room_bookings."bookingId" = bookings.id 
+          where 
+            room_bookings."isCurrent" = true 
+            and room_bookings."isDeleted" = false 
+            and bookings."isDeleted" = false
+        ) bookings_detail 
+        /* find all available rooms of this type which are not booked on these dates */
+        ON (
+          bookings_detail."roomId" = rooms.id 
+          and (
+            (
+              bookings_detail."checkInDate" <= @checkOutDate
+              AND bookings_detail."checkOutDate" >= @checkOutDate
+            ) 
+            OR (
+              bookings_detail."checkInDate" <= @checkOutDate 
+              AND bookings_detail."checkOutDate" >= @checkOutDate
+            )
+            OR 
+            (
+             bookings_detail."checkInDate"  >= @checkInDate
+             AND bookings_detail."checkOutDate"  <= @checkOutDate
+           )
+           OR 
+            (
+             bookings_detail."checkInDate"  <= @checkInDate
+             AND bookings_detail."checkOutDate"  >= @checkOutDate
+           )
+          )
+        ) 
+        /* exclude rooms with existing bookings, and fitler to only rooms of this type */
+      where 
+        rooms."roomTypeId" = @roomTypeId
+        and bookings_detail."roomId" is null 
+      order by 
+        ("gap") asc
